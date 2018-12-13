f9705f2855c1e87a1cb15541feada0cad038e9af
Ondrej Moris <omoris@redhat.com>
2018-12-13 20:45:58 +0100

    Library/fips: add missing file

---
+++ rstrnt-package-workaround.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+tmp_dir=$(mktemp -d)
+
+shift
+operation=$1
+shift 
+packages=$*
+
+if [[ "$operation" == "remove" ]]; then
+
+    dnf remove -y $packages
+    
+elif [[ "$operation" == "install" ]]; then
+
+    pushd $tmp_dir 
+    dnf install --downloadonly -y --downloaddir . $packages
+    rpm -ihv --nodigest --nofiledigest *.rpm
+    popd
+fi
+
+rm -rf $tmp_dir
